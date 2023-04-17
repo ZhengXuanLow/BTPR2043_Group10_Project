11098b0b9636f869b5117a6933fdc1074a860c82
Zheng Xuan Low <lowxuan0218@gmail.com>
2023-04-17 22:28:08 +0800

    17/4 - Done fonts, historyFunction

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 //Set the const background Color
 const bgColor = Color(0xfffafafa);
+//Set the const variable for insert and read from firestore
 const collectionName = 'history';
 const fieldName = 'qrhistory';
